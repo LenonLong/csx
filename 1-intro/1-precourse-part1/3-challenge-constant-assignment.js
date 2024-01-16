746c73f06f77dd9022324452b7dd45c10997d081
Lenon Long <amronglong@gmail.com>
2024-01-16 09:51:03 -0800

    completed csx coding problem

---
+++ 1-intro/1-precourse-part1/3-challenge-constant-assignment.js
@@ -0,0 +1,17 @@
+/*
+Challenge: Constant Assignment - (const)
+
+Ok, so now that you have a hang of var and let, let's practice declaring a constant. As we metioned in the previous lesson, 
+sometimes we want a variable who's value never changes. In this case we use the const keyword. For instance, take a look at the example below. 
+My favorite TV show is Game of Thrones, and obviously that's never going to change, so I've declared a constant myFavoriteShow 
+and assigned it a value of "Game of Thrones".
+
+const myFavoriteShow = "Game of Thrones";
+console.log(myFavoriteShow) // => "Game of Thrones"
+Maybe your favorite tv show changes quite often though, so here's a few more concrete examples of variables that won't ever change:
+*/
+// Declare a constant myCodingConfidence and assign it a value of "unwavering"
+const myCodingConfidence = 'unwavering';
+
+// Try un-commenting the line below to test your answer
+console.log("My confidence in my coding ability is " + myCodingConfidence);
