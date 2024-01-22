afcb79a45adc202660b76db347b7dae1eee19f57
Lenon Long <amronglong@gmail.com>
2024-01-22 10:56:32 -0800

    completed csx precourse problem

---
+++ 1-intro/2-precourse-part2/2-arrays-adding-elements.js
@@ -0,0 +1,13 @@
+/*
+Challenge: Arrays - Adding Elements
+
+Challenge:
+Use a built-in JavaScript method to add another show to the netflixShows array. Then log your updated array to the console
+*/
+const  netflixShows = ["Orange is the New Black", "Black Mirror", "Chewing Gum"];
+
+// ADD CODE HERE
+netflixShows.push('The Last of Us')
+
+// Write a console.log statement below to check your work!
+console.log(netflixShows)
