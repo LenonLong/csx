df432f2f0febfff64d4383b2cc7c9424d9d2b619
Lenon Long <amronglong@gmail.com>
2024-03-13 19:12:35 -0700

    completed closure problem

---
+++ 2-intermediate/2-closure-scope-execution-context/2-createFuncWithInput.js
@@ -0,0 +1,25 @@
+/*
+Challenge: createFunctionWithInput
+Create a function createFunctionWithInput that accepts one input and returns a function. When that created function is called, 
+it should return the input that was passed to createFunctionWithInput when it was created.
+
+const sampleFunc = createFunctionWithInput('sample');
+const helloFunc = createFunctionWithInput('hello');
+
+// Now we'll call the functions we created and log the result
+console.log(sampleFunc()); 
+// should log 'sample' to the console
+console.log(helloFunc());
+// should log 'hello' to the console
+*/
+// ADD CODE HERE
+const createFunctionWithInput = (arg) => {
+    return () => {
+        return arg;
+    }
+}
+// UNCOMMENT THESE TO TEST YOUR WORK!
+const sampleFunc = createFunctionWithInput('sample');
+console.log(sampleFunc()); // should log: 'sample'
+const helloFunc = createFunctionWithInput('hello');
+console.log(helloFunc()); // should log: 'hello'
